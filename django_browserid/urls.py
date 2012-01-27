ca18861250075e995d8bfa909032f202d49f61de
Owen Coutts <owen@owencoutts.com>
2012-01-26 16:12:38 -0800

    Lets use class based views

--- django_browserid/urls.py
+++ django_browserid/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls.defaults import patterns, url
 
+from django_browserid.views import Verify
+
 
 urlpatterns = patterns('',
-    url('^verify/', 'django_browserid.views.verify', name='browserid_verify')
+    url('^verify/', Verify.as_view(), name='browserid_verify')
 )
